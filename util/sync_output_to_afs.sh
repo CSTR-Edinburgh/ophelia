147ff4daee7585f29958c9986cd33d1292668ba8
jonojace <jonojace@gmail.com>
2019-02-18 16:05:32 +0000

    sync over alignment images

--- util/sync_output_to_afs.sh
+++ util/sync_output_to_afs.sh
@@ -8,7 +8,7 @@ rsync -av --relative ./work/*${PATTERN}*/train-t2m/validation_epoch_*/*.wav $DES
 rsync -av --relative ./work/*${PATTERN}*/train-ssrn/validation_epoch_*/*.wav $DEST
 rsync -av --relative ./work/*${PATTERN}*/train-t2m/alignment* $DEST
 rsync -av --relative ./work/*${PATTERN}*/synth/*/*.wav $DEST
-rsync -av --relative ./work/*${PATTERN}*/synth/*/*.wav $DEST
+rsync -av --relative ./work/*${PATTERN}*/synth/*/*.png $DEST
 
 
 echo "Synced to: $DEST"
