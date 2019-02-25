e42e9bd8b06bd96e51d03ab7d17578fb60eb9458
Oliver Watts <owatts@staffmail.ed.ac.uk>
2019-02-25 15:36:28 +0000

    depersonalised sync scripts

--- util/sync_code_from_afs.sh
+++ util/sync_code_from_afs.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-SERVERDIR=/group/project/cstr1/jasonfong/dc_tts_osw   ## group project space
-SERVERDIR2=/disk/scratch_ssd/jasonfong/dc_tts_osw   ## adamski + hynek
-SERVERDIR3=/disk/scratch_big/jasonfong/dc_tts_osw   ## lazar
+SERVERDIR=/path/on/GPU/machine/to/dc_tts_osw/   ## change this to e.g. /disk/scratch/.../dc_tts_osw
+SOURCE=/path/on/AFS/to/dc_tts_osw/  ## e.g. in your AFS home directory
 
-SOURCE=/afs/inf.ed.ac.uk/user/s17/s1785140/dc_tts_osw  ## e.g. in your AFS home directory
-
-rsync --exclude='.git/' -avzh --progress $SOURCE/ $SERVERDIR/ 
-rsync --exclude='.git/' -avzh --progress $SOURCE/ $SERVERDIR2/
-rsync --exclude='.git/' -avzh --progress $SOURCE/ $SERVERDIR3/
+rsync -avzh --progress $SOURCE/ $SERVERDIR/ 
 # --exclude='.git/'
