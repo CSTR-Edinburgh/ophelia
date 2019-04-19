2852abfd762f946e9328d0742f1c288411518f24
Oliver Watts <owatts@staffmail.ed.ac.uk>
2019-04-19 11:20:37 +0100

    alternative to gpu_lock.py

--- util/find_free_gpu.py
+++ util/find_free_gpu.py
@@ -6,7 +6,7 @@ import re
 sp = subprocess.Popen(['nvidia-smi', 'pmon', '-c', '1'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 out_str = sp.communicate()
 
-print out_str
+#print out_str
 
 lines = out_str[0].split('\n')
 lines = [line for line in lines if not line.startswith('#')] # filter comments
@@ -16,24 +16,13 @@ lines = [line for line in lines if line != ['']]               # remove empty li
 
 free_gpu = -1
 for line in lines:
-    print line
+    #print line
     gpu_id = line[0]
     process_info = ''.join(line[1:])
-    print gpu_id
-    print process_info
+    #print gpu_id
+    #print process_info
     if process_info.replace('-', '') == '': ## does it contain only  - placeholders used to mark columns for idle GPUs?
         free_gpu = gpu_id
         break
 print free_gpu
 
-# out_dict = {}
-
-# for item in out_list:
-#     try:
-#         key, val = item.split(':')
-#         key, val = key.strip(), val.strip()
-#         out_dict[key] = val
-#     except:
-#         pass
-
-# pprint.pprint(out_dict)
