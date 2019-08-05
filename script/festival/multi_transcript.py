db7fce760e2809a9b14933ac3d518a2784c6583f
pilarog <psoplust@uc.cl>
2019-08-05 23:19:20 +0100

    modified multispeaker label

--- script/festival/multi_transcript.py
+++ script/festival/multi_transcript.py
@@ -10,22 +10,13 @@ def main_work():
     a.add_argument('-o', dest='outfile', required=True)
     opts = a.parse_args()
 
-    o = open(opts.outfile, 'w')
-    
-    with open(opts.infile, 'r') as f:
-        for line in f.readlines()[:-1]:
-            if line[3] == '_':  # if clauses dealing with different length p-numbers (not really applicable for public VCTK)
-                speaker_id = line[0:3]
-            elif line[4] == '_':
-                speaker_id = line[0:4]
-            elif line[5] == '_':
-                speaker_id = line[0:5]
-            else:
-                print('Something is wrong with the input file - speaker ID cannot be parsed!')
-            o.write('{}|{}\n'.format(line.rstrip(), speaker_id))
+    outf = open(opts.outfile, 'w')
+    transcript = open(opts.infile, 'r').read().split('\n')
 
-    o.close()
+    for line in transcript:
+        spk = line.split('_')[0]
+        outf.writelines(line+'||'+spk+'\n')
+    outf.close()
 
 if __name__ == "__main__":
     main_work()
-    
