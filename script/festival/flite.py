26987b777b29acaafe287dc2d64b95c0681961d7
Oliver Watts <owatts@staffmail.ed.ac.uk>
2019-10-25 17:29:32 +0100

    ongoing work with flite phonetisation

--- script/festival/flite.py
+++ script/festival/flite.py
@@ -14,6 +14,9 @@ import os
 import re
 import subprocess
 
+from mcd import dtw  ## Matt Shannon's mcd is already a required part of the environment
+                     ## so we might as well use it for aligning phones... 
+
 HERE = os.path.realpath(os.path.abspath(os.path.dirname(__file__)))
 flite_binary = os.path.realpath(os.path.abspath(HERE + '/../../tool/flite/bin/flite'))
 
@@ -23,37 +26,7 @@ if (not os.path.isfile(flite_binary)) or (not os.access(flite_binary, os.X_OK)):
     sys.exit('!!!!!')
 
 
-def get_flite_phonetisation(textfile, dictionary='cmulex'):
-    #return '<_START_> dh ax <> b er ch <_END_>'
-
-
-    os.system("sed 's/ /, /g' %s > %s.tmp"%(textfile, textfile))
-    comm = "%s -f %s.tmp -ps none"%(flite_binary, textfile)
-    print comm
-    wordphones = subprocess.check_output(comm, shell=True)
-    # wordphones = re.split('\s+', wordphones)
-    # wordprons = []
-    # word = []
-    # for phone in wordphones:
-    #     if phone=='pau':
-    #         if word:
-    #             wordprons.append(word)
-    #         word = []
-    #     else:
-    #         word.append(phone)
-    # if word:
-    #     wordprons.append(word)
-    wordprons = re.split('\s*pau\s*', wordphones)
-    wordprons = [w for w in wordprons if w]
-
-    # print wordprons  # ['dh ax', 'f ih sh', 't w ih s t ax d', 'ae n d', 't er n d']
-
-    comm = "%s -f %s -ps none"%(flite_binary, textfile)
-    print comm
-    phones = subprocess.check_output(comm, shell=True)
-    phones = re.split('(\s*pau\s*)', phones)
-    phones = [w.strip() for w in phones if w]
-
+def simple_merge(phones, wordprons):
     print wordprons
     print phones
     #sys.exit('===ev=reb')
@@ -77,7 +50,75 @@ def get_flite_phonetisation(textfile, dictionary='cmulex'):
     print [output]
 
 
+def dtw_merge(phones, wordprons):
+    # print phones
+    # print wordprons
+    # ['pau', 'ay w aa n t ax d t ax l iy d', 'pau']
+    # ['ay', 'w aa n t ax d', 't ax', 'l iy d']
+
+    wordprons = ' <> '.join(wordprons)
+    wordprons = wordprons.split(' ')
+    phones = ' '.join(phones)
+    phones = phones.split(' ')
+
+    print phones
+    print wordprons
+
+    def cost(x, y):
+        if x==y:
+            return 0
+        elif x=='pau' and y=='<>':
+            return 0
+        else:
+            return 1
+
+    score, path = dtw.dtw(phones, wordprons, cost)
+    print path
+    output= []
+    for (phone_i, iso_i) in path:
+        print '%s %s'%(phones[phone_i], wordprons[iso_i])
+        if wordprons[iso_i] == '<>':
+            if phones[phone_i] == 'pau':
+                output.append('pau')
+            else:
+                output.append('<>')
+        else:
+            output.append(phones[phone_i])
+    print output
+
+
+def get_flite_phonetisation(textfile, dictionary='cmulex'):
+    #return '<_START_> dh ax <> b er ch <_END_>'
+
+
+    os.system("sed 's/ /, /g' %s > %s.tmp"%(textfile, textfile))
+    comm = "%s -f %s.tmp -ps none"%(flite_binary, textfile)
+    print comm
+    wordphones = subprocess.check_output(comm, shell=True)
+    wordprons = re.split('\s*pau\s*', wordphones)
+    wordprons = [w.strip() for w in wordprons if w]
+
+    # print wordprons  # ['dh ax', 'f ih sh', 't w ih s t ax d', 'ae n d', 't er n d']
+
+    comm = "%s -f %s -ps none"%(flite_binary, textfile)
+    print comm
+    phones = subprocess.check_output(comm, shell=True)
+    phones = re.split('(\s*pau\s*)', phones)
+    phones = [w.strip() for w in phones if w]
+
+
+    phones_no_pau = [w for w in phones if w != 'pau']
+    if 0: # ' '.join(phones_no_pau) == ' '.join(wordprons):
+        print 'Pron of words in context matches pronunciation of isolated words - proceed with simple merge'
+        simple_merge(phones, wordprons)
+    else:
+        print 'Back off to dynamic programming  ;-)'
+        dtw_merge(phones, wordprons)
+
+
+
 if 1: ## dev and debug
-    os.system('echo "The fish twisted and turned." > /tmp/test.txt')
+    os.system('echo "The fish; twisted and turned." > /tmp/test.txt')
+    #os.system('echo "I wanted to lead." > /tmp/test.txt')
     get_flite_phonetisation('/tmp/test.txt', dictionary='cmulex')
     sys.exit('evcrv')
